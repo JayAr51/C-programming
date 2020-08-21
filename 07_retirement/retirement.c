9f79626d5d42dcbcbb9818c4b461ad3e90f96402
Coursera Learner <nobody@nowhere.nul>
2020-08-21 23:59:19 +0000

    changed typedef

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct _retire_info{
+struct _retire_info{
   int month;
   double contribution;
   double rate_of_return;
-} retire_info;
+};
+typedef struct _retire_info  retire_info;
 
 double balanceAfterInterest(double balance,retire_info info){
   balance*=(1+info.rate_of_return);
