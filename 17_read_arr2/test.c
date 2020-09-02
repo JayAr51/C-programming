cb0232a768d474ed4c7c0a483e014377b3d5b53b
Coursera Learner <nobody@nowhere.nul>
2020-09-02 14:31:53 +0000

    commented test

--- 17_read_arr2/test.c
+++ 17_read_arr2/test.c
@@ -4,13 +4,13 @@
 
 #define NSTRS 3
 int main(void) {
-  const char * strs[NSTRS] = {"apple", "bannana", "carrot"};
+  const char * strs[NSTRS] = {"apple", "bannana", "carrot"};// defines an array of pointers, pointing to 3 different strings
   
   for (int i = 0; i < NSTRS; i++) {
-    const char * a = strchr(strs[i], 'a');
-    a++;
-    printf("%c\n", *a);
-    printf("%c\n", a[2]);
+    const char * a = strchr(strs[i], 'a');// returns a pointer to the first 'a' in strs[i] while strs[i] is a char * (also a pointer)
+    a++; // the location the the pointer points to is increased by 1
+    printf("%c\n", *a); // the next letter is printed e.g. p n or r
+    printf("%c\n", a[2]); //prints the location of the third a 
     a = strstr(strs[i], "nana");
     if (a != NULL) {
       printf("%s has %s %ld characters into it!\n", strs[i], a, a - strs[i]);
