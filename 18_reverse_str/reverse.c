a6f53996e2ed2f0634f086d6b27fe7d6a2d27938
Coursera Learner <nobody@nowhere.nul>
2020-09-02 15:55:59 +0000

    added code

--- 18_reverse_str/reverse.c
+++ 18_reverse_str/reverse.c
@@ -3,6 +3,21 @@
 #include <string.h>
 
 void reverse(char * str) {
+  // evaluating length of String
+  char *p1=str;// erstellt einen Pointer der an die Stelle des Strings zeigt
+  int n=1;
+  while (*p1!=0){
+    n++;
+    p1++;
+  } //n enthält die Anzahl an Einträgen inklusive \0
+  p1=p1-n+1;
+  char p2[n];
+  strncpy(p2, p1, n); //p2 sollte gleich p1 sein
+  size_t i;
+  for (i = 0; i < n && p2[i] != '\0'; i++){
+    p1[n-2-i]= p2[i]; //p1 wird umgekehrt und somit auch str
+  }
+  return;
   //WRITE ME!
 }
 
