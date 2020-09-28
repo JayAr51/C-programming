17b3c9206aa559995ef835196ccf62d8aa986f18
Coursera Learner <nobody@nowhere.nul>
2020-09-28 22:40:43 +0000

    now14

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 #define NLETTERS 26
-#define KEY 13
+#define KEY 14
 
 unsigned * arrayMax(unsigned * array, unsigned n){ // returns the pointer to the maximum occuring array element
   if (n<=0){
