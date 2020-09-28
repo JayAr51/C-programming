762535681199e2bb6fc2e1e05f2321bd4def8157
Coursera Learner <nobody@nowhere.nul>
2020-09-28 21:22:55 +0000

    loop count2

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -83,8 +83,9 @@ int main(int argc, char ** argv) {
   countLetters(f, letterCounts, alphabet); //letterCounts wurde erzeugt
   unsigned *MaxIdx=arrayMax(letterCounts,NLETTERS);
   if (*MaxIdx==0||MaxIdx==NULL){
+    printf("%d\n",0);
     //fprintf(stderr,"counting no aphabetic characters\n");
-    return 0;//EXIT_FAILURE;
+    return EXIT_FAILURE;
   }
   if (fclose(f) != 0) {// closeing the file and asking for errors
     perror("Failed to close the input file!");
