dbad4384aa288209cb6d4e1ebdc67144565b9564
Coursera Learner <nobody@nowhere.nul>
2020-09-28 19:54:51 +0000

    added code not binary

---
+++ 25_break_encr/breaker.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+unsigned * arrayMax(unsigned * array, unsigned n){ // returns the pointer to the maximum occuring array element
+  if (n<=0){
+  return NULL;
+  }
+  unsigned maxVal=array[0];
+  unsigned *maxIdx=&array[0];
+  int increased=0;
+  if (n==1){
+    return maxIdx;
+  }
+  for (int i=1;i<n;i++){
+    if (maxVal<array[i]){
+	maxVal=array[i];
+	maxIdx=&array[i];
+	increased++;
+      }
+  }
+  if (increased==0&&maxVal==0){
+    fprintf(stderr,"No maximum could be found\n");
+  }
+  return maxIdx;
+}
+
+int getKey(char maxletter) {
+  int key;
+  if (maxletter>='e'){
+    key=maxletter-'e';
+  }
+  else{
+    key='z'-'e'+maxletter-'a'+1;
+  }
+  return key;
+}
+
+int countLetters(FILE * f, unsigned * letterCounts, char * alphabet) {
+  int c;
+  char * letterptr;
+  unsigned idx;
+  while ((c = fgetc(f)) != EOF) {
+    if (isalpha(c)) {
+      letterptr=strchr(alphabet, c);
+      //substract pointer values and devide by 8, then increase array with resulting index by 1
+      idx=letterptr-alphabet;
+      if (idx<26){
+      letterCounts[idx]++;
+      }else{
+	printf("%c",c);
+      }
+      
+    }
+    
+  }
+  return EXIT_SUCCESS;
+}
+
+int main(int argc, char ** argv) {
+  if (argc != 2) {
+    fprintf(stderr,"Usage: breaker inputFileName\n");
+    return EXIT_FAILURE;
+  }
+  FILE * f = fopen(argv[1], "r");  //create stream
+  if (f == NULL) {
+    fprintf(stderr,"Could not open file");
+    return EXIT_FAILURE;
+  }
+  char * alphabet="abcdefghijklmnopqrstuvwxyz";
+  unsigned nletters=26;
+  unsigned letterCounts[26]={0};
+  countLetters(f, letterCounts, alphabet); //letterCounts wurde erzeugt
+  unsigned *MaxIdx=arrayMax(letterCounts,nletters);
+  if (*MaxIdx==0||MaxIdx==NULL){
+    fprintf(stderr,"counting no aphabetic characters\n");
+    return EXIT_FAILURE;
+  }
+  if (fclose(f) != 0) {// closeing the file and asking for errors
+    perror("Failed to close the input file!");
+    return EXIT_FAILURE;
+  }
+  printf("%d\n",getKey(alphabet[MaxIdx-letterCounts]));
+   return EXIT_SUCCESS;
+}
