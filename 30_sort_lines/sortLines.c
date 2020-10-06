87394306fbbc33da0e5b48555c3cd7eb59e5a55d
Coursera Learner <nobody@nowhere.nul>
2020-10-06 08:03:15 +0000

    no struct used

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-struct StringArray_ {
-  char ** array;
-  size_t numLines;
-}; typedef struct StringArray_  StringArray; 
 
 //This function is used to figure out the ordering
 //of the strings in qsort.  You do not need
@@ -19,71 +15,52 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
-StringArray readInFile(FILE *f){
+void freeData(char ** data, size_t n){
+  for (size_t i = 0; i < n; i++) {
+    free(data[i]);
+  }
+  free(data);
+}
+
+int readInFile(FILE *f){
       char * line= NULL;
       size_t sz =0;
-      StringArray s;
-      s.array = NULL;
-      s.numLines = 0;
-      while(getline(&line, &sz, f) > 0){
-	s.numLines++;
-	s.array=realloc(s.array, s.numLines * sizeof(s.array));
-	s.array[s.numLines-1] = malloc((strlen(line)+1)*sizeof(char));
-	for (size_t i = 0; i < strlen(line); i++) {
-	  s.array[s.numLines-1][i]=line[i];
+      ssize_t len=0;
+      char ** array = NULL;
+      size_t n = 0;
+      while((len=getline(&line, &sz, f)) >= 0){
+	n++;
+	array=realloc(array, n * sizeof(*array));
+	//array[n-1]=NULL;
+	array[n-1]= malloc((len+1)*sizeof(array));
+	if (array[n-1]!=0){
+	  for (size_t i = 0; i < len+1; i++) {
+	    array[n-1][i]=line[i];
+	  }
 	}
       }
       free(line);
-      sortData(s.array,s.numLines);
-      return s;
-}
-
-int writeFile(StringArray data,char * filename){
-  int success;
-  if (strcmp(filename,"stdout")==0){
-    for (size_t i = 0; i < data.numLines; i++) {
-      success=fprintf(stdout,"%s",data.array[i]);
-      //success=printf("%s",data.array[i]);
-      if (success==EXIT_FAILURE){
-	return success;
+      if (array==NULL){
+	fprintf(stderr,"empty input?");
+	freeData(array,n);
+	return EXIT_FAILURE;
       }
-    }
-    // free(data.numLines);
-  }
-  else if (strcmp(filename,"stderr")==0){
-    for (size_t i = 0; i < data.numLines; i++) {
-      success=fprintf(stderr,"%s",data.array[i]);
-      //success=printf("%s",data.array[i]);
-      if (success==EXIT_FAILURE){
-	return success;
+      sortData(array,n);
+      for (size_t i = 0; i < n; i++) {
+	printf("%s",array[i]);
       }
-    }
-    // free(data.numLines);
-  }
-  return EXIT_SUCCESS;
-}
-
-void freeData(StringArray data){
-  for (size_t i = 0; i < data.numLines; i++) {
-    free(data.array[i]);
-  }
-  free(data.array);
+      freeData(array,n);
+      return EXIT_SUCCESS;
 }
 
 int main(int argc, char ** argv) {
+  int success=EXIT_SUCCESS;
   if (argc==1){
-      StringArray data =readInFile(stdin);
-      if (data.array==NULL){
-	fprintf(stderr,"empty input?");
-	return EXIT_FAILURE;
-      }
-      int success=writeFile(data,"stdout");
-      //int success=writeFile(data,"stderr");
-      if (success==EXIT_FAILURE){
-      	fprintf(stderr,"problem writing file");
-	return EXIT_FAILURE;
-      }
-      freeData(data);
+    success=readInFile(stdin);
+    if (success==EXIT_FAILURE){
+      fprintf(stderr,"problem occurred in readFile for manual input");
+      return EXIT_FAILURE;
+    }
   }
   else{
     for (int i=1;i<argc;i++){
@@ -93,23 +70,41 @@ int main(int argc, char ** argv) {
 	  //return EXIT_FAILURE;
       }
       else{
-	StringArray data =readInFile(f);
-	if (data.array==NULL){
-	  fprintf(stderr,"problem reading file %s",argv[i]);
-	}
-	int success=writeFile(data,"stdout");
-	//int success=writeFile(data,"stderr");
+	success=readInFile(f);
 	if (success==EXIT_FAILURE){
 	  fprintf(stderr,"problem occurred in writeFile for %s",argv[i]);
 	  return EXIT_FAILURE;
 	}
-	freeData(data);
 	if (fclose(f)==EOF){
 	  fprintf(stderr,"could not close file %s",argv[i]);
-	    return EXIT_FAILURE;
+	  return EXIT_FAILURE;
 	}
       }
     }
   }
   return EXIT_SUCCESS;
 }
+//int writeFile(StringArray data,char * filename){
+  //int success=0;
+  //  if (strcmp(filename,"stdout")==0){
+  //  for (size_t i = 0; i < data.numLines; i++) {
+      //   fprintf(stdout,"%s",data.array[i]);
+//  printf("%s",data.array[i]);
+      //if (success<0){
+      //return success;
+      //}
+//  }
+    // free(data.numLines);
+    //}
+  /*else //if (strcmp(filename,"stderr")==0){
+    {for (size_t i = 0; i < data.numLines; i++) {
+      success=fprintf(stderr,"%s",data.array[i]);
+      //success=printf("%s",data.array[i]);
+      if (success<0){
+	return success;
+      }
+    }
+    // free(data.numLines);
+    }*/
+  //return EXIT_SUCCESS;
+//}
